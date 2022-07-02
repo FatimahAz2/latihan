6df9ffa40d3967e6890a2c7ad63cdf41505ee437
Yudha Irmawan <yudhairmawan.dev@gmail.com>
2022-07-02 16:03:03 +0700

    test, semua sudah selesai

--- dart_application_1/test/dart_application_1_test.dart
+++ dart_application_1/test/dart_application_1_test.dart
@@ -13,5 +13,20 @@ void main() {
       var home = 'done';
       print(testPages + home);
     });
+
+    test('SearchPage', () {
+      var search = 'done';
+      print(testPages + search);
+    });
+
+    test('FavoritePage', () {
+      var fav = 'done';
+      print(testPages + fav);
+    });
+
+    test('ProfilePage', () {
+      var profile = 'done';
+      print(testPages + profile);
+    });
   });
 }
